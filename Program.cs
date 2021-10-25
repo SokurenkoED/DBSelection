220134f340792d2a0dc18d19d2dab5586f994dcb
SokurenkoED <SokurenkoED@yandex.ru>
2021-10-25 16:51:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBSelection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
