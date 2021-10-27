3632992f4b8ed7d0ff1057e6c59794437b4ae259
SokurenkoED <SokurenkoED@yandex.ru>
2021-10-27 12:52:08 +0300

    Сделал поиск файлов по имени датчика

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,8 +34,8 @@ namespace DBSelection
 
             Console.WriteLine("Введите имя датчика.");
             string SensorName = Console.ReadLine();
-            Console.WriteLine("Введите имя файла.");
-            string FileName = Console.ReadLine();
+            //Console.WriteLine("Введите имя файла.");
+            //string FileName = Console.ReadLine();
             Console.WriteLine($"Введите время начала выборки (чч:мм:сс).");
             string TempTimeFrom = Console.ReadLine().Trim();
             string TimeFrom = ConvertDataFormat(TempTimeFrom, formatter);
@@ -42,7 +44,6 @@ namespace DBSelection
 
             #endregion
 
-
             #region Setup
 
             Dictionary<double, string> FileData = new Dictionary<double, string>();
@@ -50,43 +51,57 @@ namespace DBSelection
             string[] example;
             string[] DateArr;
             string[] DateDayArr;
-            //string FileName = SensorName.Substring(2, 3);
-            string FilePath = $"{@"06-09_07_2018\"}{FileName}{".txt"}";
+            string RuteName = SensorName.Substring(2, 3);
+            string RelatePath = "./06-09_07_2018";
+            string[] filePaths = Directory.GetFiles(RelatePath);
 
             #endregion
 
-            using (StreamReader sr = new StreamReader(FilePath, ANSI))
+            #region Запись данных в массив
+
+            foreach (string item in filePaths)
             {
-                Console.WriteLine("Идет поиск.");
-
-                string line;
-                while ((line = await sr.ReadLineAsync()) != null)
+                string filename = Path.GetFileName(item);
+                if (filename.IndexOf(RuteName) != -1)
                 {
-                    if (String.Compare(line.Split(new string[] { "\t", " " }, StringSplitOptions.RemoveEmptyEntries)[2], SensorName) == 0)
+                    using (StreamReader sr = new StreamReader($"{RelatePath}/{filename}", ANSI))
                     {
+                        Console.WriteLine($"\nИдет поиск в файле {filename}.");
 
-                        #region Обработка данных
+                        string line;
+                        while ((line = await sr.ReadLineAsync()) != null)
+                        {
+                            if (String.Compare(line.Split(new string[] { "\t", " " }, StringSplitOptions.RemoveEmptyEntries)[2], SensorName) == 0)
+                            {
 
-                        example = line.Split(new string[] { "\t", " " }, StringSplitOptions.RemoveEmptyEntries);
-                        DateArr = example[1].Trim().Replace(",",".").Split(new string[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
-                        DateDayArr = example[0].Trim().Split(new string[] { "." }, StringSplitOptions.RemoveEmptyEntries);
-                        Date = double.Parse(DateArr[0], formatter) * 3600 + double.Parse(DateArr[1], formatter) * 60 + double.Parse(DateArr[2], formatter) + (double.Parse(DateDayArr[0], formatter) - 6)*24*3600;
+                                #region Обработка данных
 
-                        #endregion
+                                example = line.Split(new string[] { "\t", " " }, StringSplitOptions.RemoveEmptyEntries);
+                                DateArr = example[1].Trim().Replace(",", ".").Split(new string[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
+                                DateDayArr = example[0].Trim().Split(new string[] { "." }, StringSplitOptions.RemoveEmptyEntries);
+                                Date = double.Parse(DateArr[0], formatter) * 3600 + double.Parse(DateArr[1], formatter) * 60 + double.Parse(DateArr[2], formatter) + (double.Parse(DateDayArr[0], formatter) - 6) * 24 * 3600;
 
-                        FileData.Add(Date, example[3]);
+                                #endregion
+
+                                FileData.Add(Date, example[3]);
+                            }
+                        }
                     }
                 }
             }
 
-            using (StreamWriter sw = new StreamWriter($"{SensorName}_{TempTimeFrom.Replace(":","-")}.dat", false, System.Text.Encoding.Default))
+            #endregion
+
+            #region Запись массива в файл
+
+            using (StreamWriter sw = new StreamWriter($"{SensorName}_{TempTimeFrom.Replace(":", "-")}.dat", false, System.Text.Encoding.Default))
             {
                 await sw.WriteLineAsync($"Time {SensorName}");
                 string LastValue = null;
                 int CountNods = 0;
                 foreach (var item in FileData)
                 {
-                    if (item.Key >= double.Parse(TimeFrom,formatter) && item.Key <= double.Parse(TimeTo,formatter))
+                    if (item.Key >= double.Parse(TimeFrom, formatter) && item.Key <= double.Parse(TimeTo, formatter))
                     {
                         LastValue = item.Value;
                         if (CountNods == 0)
@@ -107,8 +122,10 @@ namespace DBSelection
                 }
                 await sw.WriteLineAsync($"{TimeTo} {LastValue}");
             }
-            
-            Console.WriteLine("Готово!");
+
+            #endregion
+
+            Console.WriteLine($"\nГотово!");
 
         }
     }
